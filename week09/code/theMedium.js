094665917ef0f7d2295e4d7342948bd9123643e4
rebleo <rml444@nyu.edu>
2020-03-30 09:46:34 -0400

    class code

--- week09/code/theMedium.js
+++ week09/code/theMedium.js
@@ -1,1 +1,17 @@
 console.log("the message!");
+
+
+
+const theButton = document.querySelector("#firstButton");
+let theTxt = document.querySelector('h2');
+let theBody = document.querySelector('body');
+// console.log(theTxt)
+
+theButton.addEventListener('click', isClicked);
+// isClicked();
+function isClicked() {
+ console.log("clicked!");
+ theTxt.style.backgroundColor = "blue";
+ theTxt.textContent = "you turned me blue!"
+ theBody.style.backgroundColor = "yellow";
+}
