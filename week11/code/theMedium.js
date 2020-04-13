2a9af014c060ed226ec1581b2353f34b390de9ac
rebleo <rml444@nyu.edu>
2020-04-13 09:40:47 -0400

    wk11 code:

--- week11/code/theMedium.js
+++ week11/code/theMedium.js
@@ -1,1 +1,12 @@
-console.log("ES")
+console.log("ES");
+
+const theButton = $("#myButton").on("click", myFunction);
+function myFunction() {
+ console.log("click");
+ $("h3").append("<p>🌲<p>yr a hero!");
+}
+
+$("#target").click(function(){
+ console.log("click yellow!");
+ $("#other").text("Now the text is changed to this!")
+})
